5b5c853a730b63ae75b6553f045563a94e411d14
arnon19-meet <arnon19@meet.mit.edu>
2017-07-30 11:58:55 +0300

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,11 @@
+c=0
+def add_numbers():
+    c=0
+    for number in range(1,100+1):
+        print(number)
+        c=c+number
+        return(add_numbers)
+print(c)
+
+ 
+
---
+++ fun2.py
@@ -0,0 +1,12 @@
+c1='~'
+c2='!'
+x="+"
+def draw_1d(num,number):
+
+    print(c1*num)
+    print(c2*num)
+    print(x*num)
+
+draw_1d(12,c1)
+draw_1d(6,c2)
+draw_1d(24,x)
